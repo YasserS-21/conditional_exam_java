d0f8a6981d925ff9fdd7b9fe6e0a318d14404245
YasserS <56772311+YasserS-21@users.noreply.github.com>
2025-03-19 16:49:04 -0400

    remove unnecessary code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,37 +1,8 @@
 package org.example;
 
 
-import java.util.EnumMap;
-
-import static java.lang.Boolean.TRUE;
-
-/**
- * Hello world!
- *
- */
-enum MyBoolean {
-    TRUE,
-    FALSE;
-}
-
 public class App {
 
-
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
-
-        //System.out.println(checkNumber(10)); // Positive
-        //System.out.println(checkNumber(-5)); // Negative
-        //System.out.println(checkNumber(0));  //
-
-        // System.out.println(checkAge(20)); // Adult
-        // System.out.println(checkAge(16)); // Minor
-        // System.out.println(checkAge(18)); // Adult
-
-//        System.out.println(getGrades(95)); // A
-//        System.out.println(getGrades(82)); // B
-//        System.out.println(getGrades(45)); // F
-    }
     public static String checkNumber(int number) {
 
         if (number > 0) {
