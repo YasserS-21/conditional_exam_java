5e78857e486f93c4eeffdeb7ce0b2a2389cd9889
YasserS <56772311+YasserS-21@users.noreply.github.com>
2025-03-19 16:47:38 -0400

    all tests

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,21 @@
 package org.example;
 
+
+import java.util.EnumMap;
+
+import static java.lang.Boolean.TRUE;
+
 /**
  * Hello world!
  *
  */
+enum MyBoolean {
+    TRUE,
+    FALSE;
+}
+
 public class App {
-    
+
 
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
@@ -22,4 +32,45 @@ public class App {
 //        System.out.println(getGrades(82)); // B
 //        System.out.println(getGrades(45)); // F
     }
+    public static String checkNumber(int number) {
+
+        if (number > 0) {
+            return "Positive";
+        }
+
+        if (number < 0) {
+            return "Negative";
+        }
+
+        return "Zero";
+    }
+
+    private static final int AGE_TO_BECOME_ADULT = 18;
+
+    public static String checkAge(int age) {
+        if (age >= AGE_TO_BECOME_ADULT) {
+            return "Adult";
+        } else {
+            return "Minor";
+        }
+
+    }
+
+    public static String getGrades(int overallGrade) {
+
+        switch (overallGrade/10) {
+            case 10: case 9:
+                return "A";
+            case 8:
+                return "B";
+            case 7:
+                return "C";
+            case 6:
+                return "D";
+            default:
+                return "F";
+        }
+
+    }
+
 }
